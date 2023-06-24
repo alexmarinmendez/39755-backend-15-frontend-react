7d3220b9f830e12a8e94043f4bdf84a6f981e8f5
Alex Marin Mendez <alexmarinmendez@gmail.com>
2023-06-24 13:02:59 -0500

    add form

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,49 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import axios from 'axios'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [input, setInput] = useState({
+    first_name: '',
+    last_name: '',
+    email: ''
+  })
+
+  const handleInputOnChange = e => {
+    setInput(prev => ({...prev, [e.target.name]: e.target.value}))
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    try {
+      await axios('http://localhost:8080/api/users', {
+        method: 'POST',
+        data: JSON.stringify(input),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      })
+    } catch(err) {
+      console.log(err.message)
+    }
+  }
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h1>Registrar un nuevo usuario</h1>
+        <form onSubmit={handleSubmit}>
+          <label>Nombre: </label>
+          <input name="first_name" value={input.first_name} onChange={handleInputOnChange} />
+          <br />
+          <label>Apellido: </label>
+          <input name="last_name" value={input.last_name} onChange={handleInputOnChange} />
+          <br />
+          <label>Email: </label>
+          <input name="email" value={input.email} onChange={handleInputOnChange} />
+          <br />
+          <input type="submit" />
+        </form>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
